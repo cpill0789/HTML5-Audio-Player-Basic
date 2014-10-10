874c55c655d8332af26a3dcb7c0ae4215afe969e
Carl Pillot <carl.pillot@gmail.com>
2014-10-09 20:46:00 -0700

    Use json for file info

--- blocks/html5_audio_player_basic/controller.php
+++ blocks/html5_audio_player_basic/controller.php
@@ -2,9 +2,9 @@
 namespace Concrete\Package\Html5AudioPlayerBasic\Block\Html5AudioPlayerBasic;
 
 use Concrete\Core\Block\BlockController;
-use File;
 use Loader;
-
+use File;
+// use Concrete\Core\Http\Service\Json;
 class Controller extends BlockController
 {
 
@@ -43,8 +43,8 @@ class Controller extends BlockController
 
     function view()
     {
-        $this->requireAsset('javascript','jquery');
-        $this->requireAsset('javascript','jplayer');
+        $this->requireAsset('javascript', 'jquery');
+        $this->requireAsset('javascript', 'jplayer');
 
         $theme = '';
         $block = $this->getBlockObject();
@@ -52,7 +52,7 @@ class Controller extends BlockController
             $theme = $block->getBlockFilename();
         }
         if ($theme == '') {
-            $theme = 'BOOTSTRAP';
+            $theme = 'bootstrap';
         }
 
         $this->set('script', $this->getPlayerJavascript($theme));
@@ -67,19 +67,8 @@ class Controller extends BlockController
         parent::save($data);
     }
 
-    function getPlayerJavascript($playerType)
+    private function getFileInfo($f)
     {
-
-        // calculate values for player initialization
-        $isLooped = ($this->loopAudio == 1) ? "true" : "false";
-        $isAuto = ($this->autoPlay == 1) ? "true" : "false";
-        $volume = $this->initialVolume / 100;
-
-        $blockID = $this->bID;
-        $defaultAncestor = 'jp_container_' . $blockID;
-
-        $f = File::getByID($this->fID);
-
         $fileType = strtolower($f->getExtension());
 
         if ($fileType == 'ogg') {
@@ -87,57 +76,65 @@ class Controller extends BlockController
         }
 
         $relPath = $f->getRelativePath();
+
+        return array(
+            $fileType => $relPath
+        );
+    }
+
+    public function getPlayerJavascript($playerType)
+    {
+        $jh = \Core::make('helper/json');
+
+        // calculate values for player initialization
+        $isLooped = ($this->loopAudio == 1) ? "true" : "false";
+        $isAuto = ($this->autoPlay == 1) ? "true" : "false";
+        $volume = $this->initialVolume / 100;
+
+        $blockID = $this->bID;
+        $defaultAncestor = 'jp_container_' . $this->bID;
+        $defaultPlay = '';
+
         $fileTypes = $fileType;
 
-        $f2 = 0;
-        $relPath2 = '';
-        $fileType2 = '';
-
-        $title = $this->title;
+        $f = File::getByID($this->fID);
+        $info = $this->getFileInfo($f);
 
         if ($this->secondaryfID > 0) {
             $f2 = File::getByID($this->secondaryfID);
-            $relPath2 = $f2->getRelativePath();
-
-            $fileType2 = strtolower($f2->getExtension());
-
-            if ($fileType2 == 'ogg') {
-                $fileType2 = 'oga';
-            }
-            $fileTypes .= ', ' . $fileType2;
+            $info2 = $this->getFileInfo($f2);
+            array_merge($info, $info2);
         }
+
+        $fileTypes = implode(', ', array_keys($info));
+
+        $info['title'] = $this->title;
 
         if ($this->metaCategory == 'TITLE') {
-            $title = $f->getTitle();
-        }
-        if ($this->metaCategory == 'DESCRIPTION') {
-            $title = $f->getDescription();
-        }
+            $info['title'] = $f->getTitle();
+        } else
+            if ($this->metaCategory == 'DESCRIPTION') {
+                $info['title'] = $f->getDescription();
+            }
 
-        $fileInfo = '{' . $fileType . ':"' . $relPath . '",' . 'title:"' . $title . '"';
-        if ($this->secondaryfID > 0) {
-            $fileInfo .= ',' . $fileType2 . ':"' . $relPath2 . '"';
-        }
-        $fileInfo .= '}';
+        $fileInfo = $jh->encode($info);
 
         // Player event callback default values
         $defaultReady = '$(this).jPlayer("setMedia",' . $fileInfo . ');' . '$("#' . $defaultAncestor . ' .jp-current-title").html($(this).data("jPlayer").status.media.title);';
+
         if ($this->autoPlay) {
             $defaultReady .= '$(this).jPlayer("play");';
         }
 
         if ($this->pauseOthers) {
             $defaultPlay = '$(this).jPlayer("pauseOthers");';
-        } else {
-            $defaultPlay = '';
         }
-
         $defaultStandard = 'swfPath: "' . REL_DIR_PACKAGES . '/html5_audio_player_basic/flash/",' . 'supplied: "' . $fileTypes . '",' . 'wmode: "window",' . 'volume: ' . $volume . ',' . 'loop: ' . $isLooped . ',';
 
         // Assemble Player Javascript
         $playerScript = '<script type="text/javascript">$(document).ready(function(){';
 
-        if ($playerType == 'BASIC') {
+        if ($playerType == 'basic_purple') {
 
             // Javascript for Basic player
             $playerScript .= '$("#jquery_jplayer_' . $blockID . '").jPlayer({' . 'ready: function (event) { ' . $defaultReady;
@@ -145,12 +142,12 @@ class Controller extends BlockController
                 $playerScript .= '$("#' . $defaultAncestor . ' .jp-title").hide();';
             }
             $playerScript .= '},' . 'play: function(event) {' . $defaultPlay . '$("#' . $defaultAncestor . ' .jp-controls .jp-play").hide();' . '$("#' . $defaultAncestor . ' .jp-controls .jp-stop").show();' . '},' . 'pause: function (event) {' . '$("#' . $defaultAncestor . ' .jp-controls .jp-play").show();' . '$("#' . $defaultAncestor . ' .jp-controls .jp-stop").hide();' . '},' . 'ended: function () {' . '$("#' . $defaultAncestor . ' .jp-controls .jp-play").show();' . '$("#' . $defaultAncestor . ' .jp-controls .jp-stop").hide();' . '},' . $defaultStandard . 'cssSelectorAncestor: "#' . $defaultAncestor . '"' . '});' . '});</script>';
-        } elseif ($playerType == 'CIRCLE') {
+        } elseif ($playerType == 'circle_player') {
 
             // Javascript for Circle player
             $playerScript .= 'var myCirclePlayer = new CirclePlayer("#jquery_jplayer_' . $blockID . '",' . $fileInfo . ',' . '{' . $defaultStandard . 'cssSelectorAncestor: "#cp_container_' . $blockID . '"' . '});' . '});</script>';
         } else {
-            if ($playerType == 'BOOTSTRAP') {
+            if ($playerType == 'bootstrap') {
                 $defaultStandard .= "timeupdate: function(event) {
 										$('#jp_container_$blockID .jp-time-wrapper').css('left', event.jPlayer.status.currentPercentAbsolute+'%');
 									}, pause: function(event) {
