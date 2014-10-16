915ff405001dab78248caf677b63a4c3a846a519
Carl Pillot <carl.pillot@gmail.com>
2014-10-16 08:58:53 -0700

    Refactor js to get rid of initialization error.

--- blocks/html5_audio_player_basic/auto.js
+++ blocks/html5_audio_player_basic/auto.js
@@ -14,38 +14,3 @@ var JPAudioPlayerBlock = {
 }
 
 ccmValidateBlockForm = function() { return JPAudioPlayerBlock.validate(); }
-
-$(function(){
-	/* Initialize Title Source Picker */
-	var $select = $("#audioBlock-singleAudio select[name=titleSource]");
-
-	var checkTitle = function ($select) {
-		if ($select.val() == 'CUSTOM') {
-			$('#audioBlock-singleAudio #title').show();
-		} else {
-			$('#audioBlock-singleAudio #title').hide();
-		}
-	};
-
-	checkTitle($select);
-
-	$select.change(function(){
-		checkTitle($select);
-	});
-
-	/* form volume slide */
-	$( "#audioBlock-volume #audioBlock-volumeSlider").slider({
-	  min: 0,
-	  max: 100,
-	  value: $( "#audioBlock-volume input" ).val(),
-	  slide: function( event, ui ) {
-		$( "#audioBlock-volume input" ).val(ui.value);
-		$( "#audioBlock-volume #volumeLevel" ).html(ui.value);
-	  }
-	});
-	$( "#audioBlock-volume input" ).val( $( "#audioBlock-volume #audioBlock-volumeSlider" ).slider( "value" ) );
-	$( "#audioBlock-volume #volumeLevel" ).html( $( "#audioBlock-volume #audioBlock-volumeSlider" ).slider( "value" ) );
-
-	/* tooltip */
-	$( ".launch-tooltip" ).tooltip({placement: 'right'});
-});
