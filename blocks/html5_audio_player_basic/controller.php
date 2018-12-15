8250abcb22b30366dc8845f96cfca50e4476fa40
Carl Pillot <carl.pillot@gmail.com>
2018-12-15 12:43:03 -0800

    Fix bug with PHP 7.2

--- blocks/html5_audio_player_basic/controller.php
+++ blocks/html5_audio_player_basic/controller.php
@@ -17,7 +17,6 @@ class Controller extends BlockController
     protected $btCacheBlockOutput = true;
     protected $btCacheBlockOutputOnPost = true;
     protected $btCacheBlockOutputForRegisteredUsers = false;
-    protected $btCacheBlockOutputLifetime = CACHE_LIFETIME;
     protected $btDefaultSet = 'multimedia';
 
     public function getBlockTypeName()
